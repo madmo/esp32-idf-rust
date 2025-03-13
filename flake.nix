332534dc57990b12d0b501351c1973c646cb33d5
Moritz Bitsch <moritz@h6t.eu>
2025-03-13 23:40:59 +0100

    update rust toolchain

--- flake.nix
+++ flake.nix
@@ -11,8 +11,8 @@
       esp32 = pkgs.dockerTools.pullImage {
         imageName = "espressif/idf-rust";
         imageDigest =
-          "sha256:2ab58c19a031722511278b1fe65181508c116d74af0ce5b17efadd788a06b0ad";
-        sha256 = "eOBSIUcTaFQqGBoDXjgv7oe7Wxx2tF2R1MqVwfkbwXw=";
+          "sha256:0548e817d6cfccf04859bb72746a3b5425428e8da68c503dd48288e6bca13084";
+        sha256 = "3W4vjmrEY38RaCcbRmG5fyvV574vmxC0dE4HymnpTUc=";
         finalImageName = "espressif/idf-rust";
         finalImageTag = "all_latest";
       };
